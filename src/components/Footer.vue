b51f1ae9e8a43955b65c86dca0b5dab642551017
s.hamidi <sha.hamidi10@gmail.com>
2023-07-03 17:58:30 +0330

    Feature: request to weather api

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const lng = ref<any>("USflag");
+</script>
 <template>
   <a-layout-footer class="footer">
-    <div class="language-image-container">
-      <div>
-        <img :width="30" src="../assets/IRflag.png" alt="Iran flag" />
-      </div>
-      <div>
-        <img :width="30" src="../assets/USflag.png" alt="USA flag" />
-      </div>
-    </div>
+    <a-radio-group v-model:value="lng" class="language-image-container">
+      <a-radio-button class="language-button" value="IRflag"
+        ><img :width="30" src="../assets/IRflag.png" alt="Iran flag"
+      /></a-radio-button>
+      <a-radio-button value="USflag"
+        ><img :width="30" src="../assets/USflag.png" alt="USA flag"
+      /></a-radio-button>
+    </a-radio-group>
   </a-layout-footer>
 </template>
 <style scoped>
@@ -24,7 +28,7 @@
 .language-image-container {
   display: flex;
 }
-.language-image-container img {
+.language-button {
   margin: 0px 10px;
   cursor: pointer;
 }
--- src/components/WeatherApp.vue
+++ src/components/WeatherApp.vue
@@ -1,4 +1,122 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive, ref, watch } from "vue";
+import { MehOutlined } from "@ant-design/icons-vue";
+import axios from "axios";
+import { cityData } from "../cityDB/cityDb";
+interface FormState {
+  location: string;
+}
+interface Option {
+  value: string;
+  name: string;
+}
+const value = ref("");
+const cityInfo = ref<any>();
+const options = ref<any[]>(cityData);
+const formState = reactive<FormState>({
+  location: "",
+});
+watch([cityInfo], () => console.log(cityInfo.value.name));
+const onFinish = ({ location }: any) => {
+  axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=2ad622963b956f73c2e0395cb01fad6b`
+    )
+    .then((response) => {
+      cityInfo.value = response.data;
+    });
+};
+const filterOption = (input: string, option: Option) => {
+  return option.value.toUpperCase().indexOf(input.toUpperCase()) >= 0;
+};
+</script>
 <template>
-  <h1>hello</h1>
+  <a-form :model="formState" name="basic" autocomplete="off" @finish="onFinish">
+    <a-layout-content class="weather-app-layout-content">
+      <a-row type="flex" justify="center">
+        <a-col :span="9" style="margin: 0px 20px" :xs="24" :md="18" :lg="12">
+          <a-form-item
+            name="location"
+            :rules="[{ required: true, message: 'Please select a location!' }]"
+          >
+            <a-auto-complete
+              v-model:value="formState.location"
+              :options="options"
+              placeholder="Select a location"
+              :filter-option="filterOption"
+            />
+          </a-form-item>
+        </a-col>
+        <a-col
+          :xs="24"
+          :md="18"
+          :lg="12"
+          :span="1"
+          :style="{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }"
+        >
+          <a-button type="primary" html-type="submit">Fetch weather</a-button>
+        </a-col>
+      </a-row>
+      <a-row
+        v-if="cityInfo?.name"
+        type="flex"
+        justify="center"
+        class="list-container"
+      >
+        <a-col
+          :span="10"
+          :xs="24"
+          :md="20"
+          :lg="12"
+          v-bind:class="
+            cityInfo.main.temp > 75 ? 'with-data-container' : 'sssss'
+          "
+        >
+          <h1 :style="{ marginTop: '10px', fontWeight: 'bold' }">
+            {{ cityInfo.name }}
+          </h1>
+        </a-col>
+      </a-row>
+      <a-row v-else type="flex" justify="center" class="list-container">
+        <a-col :span="10" :xs="24" :md="20" :lg="12" class="no-data-container"
+          ><div><meh-outlined style="font-size: x-large" /></div>
+          <span style="margin-top: 10px">No data to show</span>
+        </a-col>
+      </a-row>
+    </a-layout-content>
+  </a-form>
 </template>
+<style scoped>
+.with-data-container {
+  background-color: rgba(46, 73, 163, 0.1);
+  overflow: auto !important;
+  border-radius: 0.5rem;
+  max-height: 450px;
+  padding: 1rem;
+}
+.weather-app-layout-content {
+  background: #fff;
+  margin: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 24px;
+}
+.no-data-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-size: 20px;
+  background-color: rgba(46, 73, 163, 0.1);
+}
+.list-container {
+  margin-top: 10px;
+  min-height: 300px;
+  max-height: 400px;
+}
+</style>
